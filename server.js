9090f00d413d88e486bb177454c593e88acee791
walusoma <walusomam@gmail.com>
2026-09-05 00:06:55 +0300

    Add categories page, refactor route standards, and apply responsive CSS

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ app.get('/projects', async (req, res) => {
     res.render('projects', { title });
 });
 
+app.get('/categories', async (req, res) => {
+    const title = 'Service Project Categories';
+    res.render('categories', { title });
+});
+
 // This starts the server so it listens for requests on port 3000
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
