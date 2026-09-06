56f73c004ed53ca79056b7c58e90e0f132860096
walusoma <walusomam@gmail.com>
2026-09-07 01:13:10 +0300

    Fix SSL self-signed certificate error for Render deployment

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: process.env.DB_URL && !process.env.DB_URL.includes('localhost')
+        ? { rejectUnauthorized: false }
+        : false
 });
 
 /**
